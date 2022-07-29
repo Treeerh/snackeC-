7ed1ad9017fbe2b9384ab5a6e82011037012c860
treeth <kolisto.07@gmail.com>
2022-07-29 20:00:25 +0300

    перевнли все в класс

--- snacke/snacke/Program.cs
+++ snacke/snacke/Program.cs
@@ -10,27 +10,21 @@ namespace snacke
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char symb1 = '*';
-            /*вызов ф-и*/
-            Draw(x1,y1,symb1);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.symb = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 7;
+            p2.symb = '#';
+            p2.Draw();
             
-            int x2 = 4;
-            int y2 = 5;
-            char symb2 = '#';
-
-            Draw(x2, y2, symb2);
-
             Console.ReadLine();
 
-
         }
-        /*Функция вывода точки на экран*/
-        static void Draw(int x, int y, char symb)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(symb);
-        }
+ 
     }
 }
